d4130837b9c38b003b85bac46db177608e2c8d1e
admin <>
2018-10-23 17:15:09 +0530

    {/policyquote/src/main/java/org/acme/insurance/Driver.java}

--- policyquote/src/main/java/org/acme/insurance/Driver.java
+++ policyquote/src/main/java/org/acme/insurance/Driver.java
@@ -9,8 +9,25 @@ public class Driver implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   private java.lang.String name;
+
    public Driver()
    {
+   }
+
+   public java.lang.String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(java.lang.String name)
+   {
+      this.name = name;
+   }
+
+   public Driver(java.lang.String name)
+   {
+      this.name = name;
    }
 
 }
